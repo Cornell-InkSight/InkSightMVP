5edefcdd11ba18ebea83dbde8df40b3ded3b3da1
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2024-10-31 22:46:27 -0400

    done

--- InkSightMVP/InkSightMVP/settings.py
+++ InkSightMVP/InkSightMVP/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'notetakingrequestmanagement',
     'schoolmanagement',
     'usermanagement',
+    'lecturesessionsmanagement',
+    'notepacketsmanagement',
+    'permissionsmanagement',
 ]
 
 MIDDLEWARE = [
--- InkSightMVP/notepacketsmanagement/models.py
+++ InkSightMVP/notepacketsmanagement/models.py
@@ -5,8 +5,8 @@ from usermanagement.models import Professor, TeacherAssistant
 class NotesPacket(models.Model):
     """Model for creating Notes Packet"""
     id = models.AutoField(primary_key=True)
-    notes = models.JSONField(primary_key=True)
+    notes = models.JSONField()
     student_course = models.ForeignKey(StudentCourse, on_delete=models.CASCADE)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     teahcher_assistant = models.ForeignKey(TeacherAssistant, on_delete=models.CASCADE)
-    
+
