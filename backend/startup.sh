e13d0cbce8b42a7cc91a9e97a57e853d464fb2d5
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2024-11-17 18:43:47 -0500

    bro

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+source venv/bin/activate
+
+# Run Django migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the Gunicorn server
+gunicorn InkSightMVP.wsgi:application --bind 0.0.0.0:8000
