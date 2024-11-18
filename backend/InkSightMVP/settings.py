908938b293c31d960a51d7e933500e9e3fb5afc4
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2024-11-18 14:09:17 -0500

    whats up my sistas

--- backend/InkSightMVP/settings.py
+++ backend/InkSightMVP/settings.py
@@ -29,7 +29,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env.local'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY",  default="default_secret_key")
+
+if SECRET_KEY == "default_secret_key":
+    print("WARNING: Using default SECRET_KEY. Check if DJANGO_SECRET_KEY is being loaded from the environment.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
